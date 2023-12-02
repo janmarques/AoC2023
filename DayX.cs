db70362c377d2180a167bebc1357a30f3befd95e
Jan Marques <jan@janmarques.net>
2023-12-02 10:00:34 +0100

    day 2 init

---
+++ DayX.cs
@@ -0,0 +1,16 @@
+﻿//var fullInput =
+//@"";
+
+//var smallInput =
+//@"";
+
+//var smallest = "";
+
+//var input = smallInput;
+////var input = fullInput;
+////var input = smallest;
+
+//var result = "";
+
+//Console.WriteLine(result);
+//Console.ReadLine();
