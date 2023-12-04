0e6ca5521aad86946c2506112ce82a58e20a8840
Jan Marques <Jan.Marques@mediahuis.be>
2023-12-04 10:36:59 +0100

    pt 2 start

--- Program.cs
+++ Program.cs
@@ -216,7 +216,7 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
 
 var smallest = "";
 
-//var input = smallInput;
+//var input = smallInput; 
 var input = fullInput;
 //var input = smallest;
 var timer = System.Diagnostics.Stopwatch.StartNew();
