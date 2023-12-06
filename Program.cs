0b9c6ccf5d1fc8234f1c35be6eebad91b17c2616
Jan Marques <Jan.Marques@mediahuis.be>
2023-12-06 10:43:02 +0100

    pt1

--- Program.cs
+++ Program.cs
@@ -21,12 +21,37 @@ foreach (var line in input.Split(Environment.NewLine))
 }
 
 var sampleRaces = new List<Race> {
-    new Race { Time = 7, Distance    = 9},
-    new Race { Time = 15, Distance    = 40},
-    new Race { Time = 30, Distance    = 200},
+    new Race { Time = 7, Distance = 9},
+    new Race { Time = 15, Distance = 40},
+    new Race { Time = 30, Distance = 200},
+};
+
+
+var realRaces = new List<Race> {
+    new Race { Time = 46, Distance = 347},
+    new Race { Time = 82, Distance = 1522},
+    new Race { Time = 84, Distance = 1406},
+    new Race { Time = 79, Distance = 1471},
+};
+
+var races = sampleRaces;
+races = realRaces;
+
+var product = 1;
+foreach (var race in races)
+{
+    for (int i = 1; i < race.Time; i++)
+    {
+        var distance = i * (race.Time - i);
+        if (distance > race.Distance)
+        {
+            race.Wins++;
+        }
+    }
+    product *= race.Wins;
 }
 
-Console.WriteLine(result);
+Console.WriteLine(product);
 
 timer.Stop();
 Console.WriteLine(timer.ElapsedMilliseconds + "ms");
@@ -48,4 +73,5 @@ class Race
 {
     public int Time { get; set; }
     public int Distance { get; set; }
+    public int Wins { get; set; }
 }
