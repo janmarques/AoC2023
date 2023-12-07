6114ef33bc1dab01cc1e2bb7f1433b4bd02c1efa
Jan Marques <Jan.Marques@mediahuis.be>
2023-12-07 07:54:58 +0100

    pt 1 setup

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-
-var fullInput =
+﻿var fullInput =
 @"";
 
 var smallInput =
@@ -15,20 +13,9 @@ var timer = System.Diagnostics.Stopwatch.StartNew();
 
 var result = 0;
 
-var T = 46828479;
-var D = 347152214061471;
-
-var disc = Math.Sqrt(Math.Pow(T, 2) + 4 * D);
-var zero1 = (T + disc) / 2;
-var zero2 = (T - disc) / 2;
-
-for (long i = 1; i < 46828479; i++)
+foreach (var line in input.Split(Environment.NewLine))
 {
-    var distance = i * (46828479 - i);
-    if (distance > 347152214061471)
-    {
-        result++;
-    }
+
 }
 
 Console.WriteLine(result);
@@ -36,8 +23,6 @@ Console.WriteLine(result);
 timer.Stop();
 Console.WriteLine(timer.ElapsedMilliseconds + "ms");
 Console.ReadLine();
-//28360140
-//804ms
 
 void PrintGrid<T>(T[][] grid)
 {
@@ -49,11 +34,4 @@ void PrintGrid<T>(T[][] grid)
         }
         Console.WriteLine();
     }
-}
-
-class Race
-{
-    public long Time { get; set; }
-    public long Distance { get; set; }
-    public long Wins { get; set; }
 }
