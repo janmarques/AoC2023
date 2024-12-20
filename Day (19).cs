9ecf8f92774f3a1b672c8698e82fa934a97abcdb
Jan Marques <jan@janmarques.net>
2024-12-20 22:26:43 +0100

    ok

--- Day (19).cs
+++ Day (19).cs
@@ -4,33 +4,28 @@ using System.Runtime.Intrinsics.Arm;
 
 var fullInput =
 @"kbr{x>2309:A,R}
-rp{a>2160:mcm,m<3139:bz,fs}
+rp{a>2160:R,m<3139:bz,fs}
 hqr{a>3048:R,x>1298:R,R}
 ht{s<1042:A,a<3744:A,a>3879:lb,zsj}
-hkl{x<2690:xps,gsq}
 bvk{s<464:R,x>1190:R,a<3443:A,A}
-mj{a<725:R,R}
-kph{s<385:A,A}
 vr{x>1596:xt,nk}
 hc{m<999:A,x<224:R,R}
 bp{a>1785:msj,sk}
 fs{a<1989:A,x>561:kdv,x>192:A,R}
 nls{x>3679:R,s<1583:A,s<1669:A,A}
-gsq{a>1760:A,A}
 phd{a<187:R,a>307:A,s>2783:A,A}
 nb{s<238:A,R}
 rs{x<2853:cd,a<1366:pxm,m>2215:cfp,bpt}
 cvn{m>1940:tb,s<421:tmg,s>560:nnq,A}
 fh{x<264:tf,hvg}
 kl{x>382:R,s>1772:R,s>1604:R,R}
-hz{s<1084:A,A}
 sdm{x<725:hpj,bd}
 xm{s<1235:vbs,s>1363:A,mh}
 xb{m<1640:jn,x<3607:A,pnq}
 zlj{a>3273:A,x<1274:A,R}
 bd{x<755:vc,cz}
 qdv{x<350:cgv,s<2917:zrb,kks}
-qsc{s>301:xs,a<1732:R,m<3313:A,A}
+qsc{s>301:R,a<1732:R,m<3313:A,A}
 rt{x<160:A,s>2931:R,R}
 dzm{m<1790:R,m<1939:A,s>2295:A,A}
 zsx{a>2296:xfj,jbd}
@@ -49,11 +44,9 @@ tbx{x<2370:tgp,a<1359:nsd,x>2467:bhj,lxg}
 ffd{a<642:R,a<845:hfj,R}
 jgb{m<3251:R,s<224:R,m>3513:R,A}
 lc{x<2011:R,x<2110:R,R}
-dnk{m>3033:A,A}
 fhq{a>1557:A,s<3161:rkj,s>3607:djm,ggh}
 glk{m>2388:rt,gcb}
 kb{m>2412:jnq,x<266:A,vq}
-tq{m<1435:A,A}
 tff{s<279:R,A}
 nz{s<335:fvn,a<358:mpp,s>587:xd,jqc}
 vc{x>735:R,a<2284:R,s<3520:R,R}
@@ -67,12 +60,12 @@ jmr{x>522:R,x>316:A,s>787:A,R}
 zqq{m<1496:A,x<112:A,R}
 vbs{m<1219:R,s>1032:R,s>919:R,A}
 dlv{s<707:A,a<446:A,s>741:kc,R}
-lt{x<3238:R,x<3353:A,zg}
+lt{x<3238:R,x<3353:A,A}
 glh{a<1492:pgv,hd}
 ds{s<610:A,R}
 bvz{a<3024:nzm,a<3669:shb,m<1829:jxg,A}
 tl{s>3142:R,s<2376:bq,x<1364:A,qt}
-zn{m>2958:tk,bs}
+zn{m>2958:R,bs}
 pvd{x>2669:R,A}
 tp{a<2819:bh,gvz}
 zv{s<619:km,R}
@@ -86,15 +79,13 @@ xq{m>1452:R,a>1510:A,A}
 zb{s<2772:R,A}
 kv{s<2620:A,s<2650:A,R}
 ksd{x>2528:R,rxz}
-tf{a<1047:stx,m<2657:js,pcb}
+tf{a<1047:stx,m<2657:R,pcb}
 dcf{x>397:A,x<367:A,R}
-gs{x>545:R,R}
 nv{m<2534:R,x>1297:A,m<3435:dc,dsh}
 dhz{x<579:A,m<3496:R,a>1496:R,A}
 rpk{a<3529:R,x>2673:R,m<3343:A,A}
 vrk{s<746:xph,s<771:nhf,jmr}
 jtb{x>2319:R,a<856:R,kbr}
-bnd{x>355:R,R}
 cfc{m>2900:R,A}
 hvg{x<356:A,a>1092:fj,s<451:A,A}
 xfj{x>659:jnv,s<2767:kj,s<2843:A,R}
@@ -102,7 +93,7 @@ xj{s>3265:R,x<1251:R,a>3192:A,A}
 kzf{m<2460:R,s<1082:tqp,a>2659:dcr,A}
 bnm{m<3230:gz,a>843:dhz,vbz}
 jn{s>2515:R,A}
-hb{m>2503:ttf,a>3916:A,qj}
+hb{m>2503:R,a>3916:A,qj}
 rq{s<1167:xfg,s<1386:R,x<3821:nls,R}
 rv{x<2349:zpb,a<1231:zfm,R}
 djm{x<3262:R,A}
@@ -121,14 +112,12 @@ dxc{m>616:hkn,s>2680:cld,x>566:vf,jfh}
 zrt{s>2756:A,R}
 cr{m>3260:R,gkr}
 sd{a<2207:jq,a<3385:kzf,x>1721:ht,nv}
-vs{x<451:R,R}
-qpr{m>3272:vs,fkm}
+qpr{m>3272:R,fkm}
 shb{m<2618:R,m<3334:R,A}
 rlc{x<305:R,x>321:R,m>3127:A,A}
 qj{x>2660:A,x>2348:R,m<884:A,R}
 djd{x<2495:gfs,htj}
 hht{a>3137:A,x<649:R,A}
-pr{m<662:R,R}
 tsf{m<1703:A,a<1358:R,A}
 lz{a<952:R,A}
 gv{a>3238:R,x>453:A,s<2711:R,R}
@@ -137,7 +126,6 @@ kmq{x>392:zn,jb}
 hn{s<1515:xm,kl}
 np{a>2419:A,m>553:A,A}
 nr{a>3070:xgb,ngg}
-ktg{s>2571:A,A}
 qvh{a>2488:R,a>918:A,A}
 stx{s>289:A,x>91:R,s<156:A,R}
 hqd{a<632:A,x>2009:A,A}
@@ -146,7 +134,7 @@ pzh{m<2360:md,m>2979:rqr,s>2988:br,krv}
 zrb{s<2528:tz,m>1549:zsx,a<1491:dxc,dq}
 llb{m<1107:djz,s>300:R,A}
 cgv{a>1941:glk,x>147:zs,m<1869:gqf,vk}
-xrl{s>2706:A,m<561:hnr,m>748:A,knh}
+xrl{s>2706:A,m<561:hnr,m>748:A,A}
 xfg{x>3831:R,a<978:A,A}
 vdf{s>3024:A,a>840:A,R}
 tmg{m<1656:R,a<1767:A,x>696:R,A}
@@ -155,12 +143,10 @@ nl{s>3402:A,s<3260:R,R}
 jtq{a>2199:nr,qd}
 jjq{s>2520:xj,m<713:rm,m<1281:R,hqr}
 mv{s>2678:A,x>2760:A,R}
-rrn{m>772:A,A}
 ngg{a<2529:gmf,x>3082:cvp,tp}
 bkr{m<3088:rfh,R}
-tgp{x<2296:pqx,x<2337:jtb,rv}
+tgp{x<2296:A,x<2337:jtb,rv}
 ncf{x>196:R,s<2752:R,A}
-vp{m<2026:A,A}
 fx{s>1162:cl,df}
 hr{m>954:hn,spf}
 nnq{a<1542:A,R}
@@ -168,15 +154,13 @@ tdn{x<2514:R,s<646:R,A}
 gqf{a<707:hh,m>956:ldg,nxv}
 mkt{s>727:sd,a>2466:rmg,a<988:rl,bp}
 nzb{x<2686:R,a<1081:A,x<2792:A,A}
-js{a<1188:R,R}
 lm{x>1189:R,x>1176:A,A}
 vz{x<3651:R,x>3854:R,R}
 bqq{x>1279:R,s>3370:R,x>1202:A,R}
 snv{x<194:A,x<252:A,a>235:A,R}
 msv{a<592:A,a<707:A,A}
 stg{s>1846:R,x<683:R,m>2553:R,A}
-rl{m>1908:hns,s<321:pkh,ffd}
-nnd{m<2005:A,A}
+rl{m>1908:hns,s<321:R,ffd}
 zpb{s>591:A,R}
 jnv{m>2507:A,s<2765:A,x>797:A,A}
 llz{a<835:R,x>406:A,x<377:R,A}
@@ -189,7 +173,6 @@ bq{x<1438:A,m<3141:A,R}
 zts{a<1415:A,s<3560:R,A}
 qgk{s>3620:A,m<2494:A,m>2665:A,A}
 zqp{s>252:A,s<96:cm,s<186:hhp,R}
-kmr{a<517:A,A}
 nt{a>419:msv,phd}
 ltc{s<1274:zxr,djq}
 psc{x<2433:ds,s>848:R,s>450:fln,nb}
@@ -197,7 +180,6 @@ nx{m>3177:R,lqj}
 hk{m>2677:R,m<2443:A,a>3006:R,R}
 rmg{x>1381:nq,m>2136:rh,bvk}
 pgv{x<834:ng,s>3456:R,hjb}
-klc{s<3005:A,A}
 zqh{s<615:A,x<2462:A,R}
 skb{x<758:ph,R}
 kx{s>2301:R,m<2849:R,x<1374:A,R}
@@ -220,7 +202,7 @@ qn{s<2335:mdr,x<3486:tmt,ct}
 jhq{a<2243:R,A}
 pkz{a<433:R,a>626:xzr,qm}
 mh{m>1320:R,A}
-mdb{s>542:R,m<3263:A,s<470:A,nxf}
+mdb{s>542:R,m<3263:A,s<470:A,A}
 kjt{s<535:A,snv}
 ghz{x>1739:xq,tsf}
 hkg{s<304:R,m<1547:R,a>1657:R,R}
@@ -241,10 +223,9 @@ ll{s<1434:R,A}
 zc{m<2298:gk,a>1836:pbc,a<679:nz,mrs}
 nrx{x>512:cvn,pht}
 jp{m>3379:A,s<2764:kx,mhk}
-dtm{a<2310:jhq,x>3498:pr,R}
+dtm{a<2310:jhq,x>3498:R,R}
 jtd{x<683:R,a>1267:R,x>716:R,A}
 cfp{a>1662:bjn,m>2877:fhq,x<3511:hkz,lnf}
-fgh{a<868:A,A}
 fj{s>498:A,x>470:R,s<310:A,A}
 jqc{x>351:sls,x>175:vgf,x<78:xg,dr}
 znr{x<3162:A,xl}
@@ -259,7 +240,7 @@ pmn{x>191:A,m<3098:A,x>111:vt,A}
 cz{a<2649:A,x<764:A,R}
 vtb{s>444:A,s<391:R,A}
 jb{a>1498:R,a>1420:A,m<3224:R,A}
-hh{a>280:klc,a>108:R,A}
+hh{a>280:A,a>108:R,A}
 vq{m<2056:R,a<1210:A,A}
 tmt{a>3804:R,m>2629:R,s>3327:bjd,A}
 fvn{s>186:bkr,qpr}
@@ -274,10 +255,7 @@ hjb{x>854:R,m>1887:R,s<3244:A,A}
 fgb{s<768:glp,s>1396:ppx,xz}
 qm{a<505:R,s<2904:A,a>547:R,R}
 zhl{x>2825:A,m<510:R,a>2370:A,R}
-pkh{s>164:R,R}
-zxr{m>2980:A,m<2494:gfc,pnl}
-ttf{x>2552:R,R}
-mcm{m>3429:R,R}
+zxr{m>2980:A,m<2494:gfc,R}
 jbj{a>1917:A,R}
 ss{a<1217:A,R}
 zfm{m<1550:R,a>576:R,R}
@@ -286,24 +264,18 @@ zr{a<1925:sv,vtm}
 rjc{a>2606:R,s>584:gt,m<1044:A,lz}
 vk{m<3006:sn,srl}
 tt{s>3218:zts,R}
-lnf{m>2546:lqd,a>1530:R,x<3726:A,grm}
+lnf{m>2546:R,a>1530:R,x<3726:A,grm}
 tcv{x>2714:R,m<1245:R,m>1589:A,R}
 tv{s>2968:A,s>2227:nzb,s>1962:R,R}
-zg{m<3262:A,A}
-srf{m>1444:R,R}
 bhj{m>1827:djd,xn}
 ctk{x<2502:R,m>644:A,A}
 fdj{x>82:R,a>2339:A,a<1175:R,rfr}
 pll{s<1161:ls,R}
-xs{m<3011:R,R}
 rh{a>3077:qbp,lh}
 sz{x<3112:R,A}
-tqv{x<639:sxl,a<1128:bl,a>1226:jtd,tj}
-knh{a>2375:A,A}
+tqv{x<639:R,a<1128:bl,a>1226:jtd,tj}
 jrk{s>960:A,m>1411:A,m<810:R,A}
 lr{x<385:R,s>1208:R,a<1017:R,R}
-slf{s<653:R,bnd}
-ctz{x<561:R,R}
 hkn{s>2693:R,s<2603:R,R}
 lxg{a<1912:psc,mb}
 kh{x>1524:vr,x>1443:hzx,hl}
@@ -322,32 +294,29 @@ bjn{a>1941:vmt,m<3140:A,A}
 vgf{a<571:R,s>464:mg,x>262:rlc,tbz}
 krh{a<1475:R,s>2407:qs,R}
 dc{a>3658:R,A}
-nbg{s>739:A,A}
 ks{m<1544:A,s<3037:A,A}
-cdv{s<2079:xx,a<1586:A,x>541:nnd,zlc}
+cdv{s<2079:xx,a<1586:A,x>541:A,zlc}
 sls{m<3378:A,m>3587:vtb,m>3468:A,R}
 gl{m>1579:cn,m>824:R,x<2504:A,zhl}
 gvj{s>3622:A,R}
 xkv{m<830:A,rtp}
 rft{x>2825:nqj,s>873:A,R}
 gh{x<1450:R,x<1708:A,a>1309:A,A}
-nxf{s<512:A,A}
 npj{x<769:R,A}
 lb{a<3931:A,m>2440:A,A}
-dnm{x<2845:hkl,a>1857:hz,ff}
+dnm{x<2845:A,a>1857:A,ff}
 djz{s<165:R,a>1810:R,m<838:R,A}
 rhs{s>1290:R,s<1113:A,lr}
 jnf{x>260:vm,m>896:A,ncf}
 kvv{s>2083:kcp,a>1714:dtp,R}
 qlf{m>691:llb,fk}
 jnl{s<625:lcb,a<1260:nlg,dnm}
-cld{x>616:gc,x<481:llz,s>2797:kmr,zrt}
+cld{x>616:gc,x<481:llz,s>2797:A,zrt}
 vt{a>3526:R,m<3679:R,A}
 df{m<2933:R,m>3533:R,R}
 gc{m>240:R,s<2813:R,s<2876:R,R}
 rb{m<1705:gf,xqx}
-fn{s<1540:srf,s<1638:R,a>720:R,A}
-xps{a<1670:A,A}
+fn{s<1540:R,s<1638:R,a>720:R,A}
 kj{m<2405:R,a>3278:A,m>3079:A,A}
 zkl{a>2556:A,R}
 km{x<723:R,x<811:A,s>533:R,R}
@@ -358,11 +327,10 @@ qd{s>1717:rs,x>3039:pxd,x>2517:jnl,tbx}
 kcp{m<1832:A,x>3478:A,a>1875:A,A}
 cn{m<3187:R,A}
 xn{x<2498:nfs,x>2510:tdn,a>1755:bhl,ctk}
-jq{m>1864:dnk,R}
+jq{m>1864:A,R}
 fln{x>2453:R,A}
 bls{a<903:vhs,a>1163:R,s>1324:qjb,vzd}
 xg{s>428:R,A}
-pqx{s<951:vp,fgh}
 zlc{x>424:A,s>2117:R,R}
 fk{x>717:qkz,A}
 hkz{s<2594:sz,a<1529:A,s>3242:qgk,A}
@@ -372,11 +340,10 @@ sj{a<2577:R,x<214:A,m>658:A,R}
 nqj{x>2968:A,x>2918:A,s<879:A,A}
 zjm{a>1201:A,a>673:R,m>2117:A,R}
 vd{x>2421:A,R}
-pl{a>1664:smh,a<1487:A,m>1282:qln,rrn}
+pl{a>1664:smh,a<1487:A,m>1282:qln,A}
 flb{m<2504:R,A}
-njz{x>2495:R,s>2145:tgm,A}
+njz{x>2495:R,s>2145:A,A}
 lh{a>2770:R,x>1154:A,A}
-tk{s>451:R,R}
 nhf{a>526:R,s>758:A,m<2708:R,R}
 nlg{s>1325:fn,s>969:sjv,s>764:rft,dlv}
 mhk{s<3388:A,s<3786:A,R}
@@ -390,25 +357,23 @@ vrv{m>1132:A,m<471:A,x<2426:A,A}
 tjx{s>1411:R,xv}
 dtd{s<1726:rqj,R}
 tb{m<2099:A,s>391:A,a<1891:R,R}
-pvz{x>116:R,R}
-jfh{s>2621:mth,x>463:ktg,R}
+jfh{s>2621:mth,x>463:A,R}
 tpq{m<3404:R,s<338:A,A}
 mvc{s<554:A,R}
-vpx{s<3376:R,m>1797:gvj,xqt}
+vpx{s<3376:R,m>1797:gvj,A}
 pxd{a>1203:jdb,x<3509:fgb,kt}
 xv{s>1206:R,a>273:A,a<178:A,A}
 vtm{m>740:hc,tff}
-qsf{s<2344:R,R}
 glp{x>3319:tbp,a>404:jss,A}
-ct{a<3801:tq,vcd}
+ct{a<3801:A,vcd}
 kz{x>2544:R,x>2412:A,R}
 rcv{x>322:rg,a<1629:tjq,m<481:ll,sj}
-hx{m<841:bkf,rjc}
-sn{x>74:mj,m<2277:zjm,R}
+hx{m<841:A,rjc}
+sn{x>74:R,m<2277:zjm,R}
 qbp{s<334:A,R}
 gk{m>1426:nrx,s>462:hx,x<410:zr,qlf}
 ls{s>887:A,m>1764:A,a<1743:R,A}
-qz{a<3051:qjd,a<3419:R,x>525:R,qsf}
+qz{a<3051:qjd,a<3419:R,x>525:R,R}
 td{a<930:R,A}
 tbz{x>225:A,s>420:R,a<636:A,A}
 sg{m<764:A,R}
@@ -423,7 +388,7 @@ jlx{a>1059:A,s>391:tcv,m>1270:R,sg}
 xt{x>1629:R,x<1615:A,R}
 bf{x>2021:A,m<2308:R,x>1936:R,R}
 xhj{m<2273:A,R}
-gsh{a<934:krd,m<3494:R,s<173:qvb,R}
+gsh{a<934:krd,m<3494:R,s<173:A,R}
 ldg{x<59:ks,zqq}
 qkz{a>2128:R,A}
 gf{s<2815:xgs,a>1355:lc,hqd}
@@ -431,34 +396,30 @@ rtp{m<1763:A,R}
 sk{m>2350:R,s<472:gh,m<1250:A,A}
 bs{m>2556:A,R}
 sjv{a>608:R,a>295:R,A}
-mpp{x>306:mdb,m<2999:pvz,kjt}
+mpp{x>306:mdb,m<2999:R,kjt}
 gvz{a<2936:nm,a<2997:A,kz}
 cm{x<522:R,A}
-qvb{a>1114:A,A}
 msj{a>2017:R,jbj}
-vj{m>2493:kph,R}
+vj{m>2493:A,R}
 khk{a>2015:jjq,s>2578:jl,lk}
 gfc{x>329:R,x>180:A,R}
 nxv{a<1256:R,scf}
 tvr{m<1620:hr,zgh}
 spf{x>431:np,x>159:rcv,fdj}
-tgm{a<3808:A,A}
 vv{s>470:A,x>1559:R,R}
 bhl{m>1174:R,A}
 cvp{a<2732:dtd,xb}
 vm{x<307:R,m<674:A,A}
 rxz{a<3704:A,R}
 vhs{m>1903:A,R}
-dg{a>1981:vpx,m<2484:td,x>492:bnm,sfz}
+dg{a>1981:vpx,m<2484:td,x>492:bnm,A}
 grm{m<2343:A,m<2453:A,m>2515:A,R}
-bkf{m>381:A,A}
 lqg{a>3849:hb,a>3763:njz,s<1979:nc,ksd}
 lv{x>873:vh,s>1928:qdv,s<801:zc,tvr}
 bjd{m>1439:R,x>3274:A,x<3190:A,A}
 htv{s>632:lpq,x>3727:R,jgb}
 gz{m<2910:A,m<3037:R,m>3112:R,R}
 gcb{s<3266:R,A}
-pnl{m>2775:R,R}
 zgh{a>1712:ltc,x>460:rfj,xh}
 rfj{a>1129:dgr,s<1540:fx,cpn}
 gt{s<664:R,x<345:A,R}
@@ -466,7 +427,6 @@ db{a>805:R,a>320:R,zb}
 pnq{m>3118:A,x>3840:A,R}
 nk{s<2788:R,s>3293:A,s>3099:A,A}
 sp{x<3298:A,x<3443:fc,hkg}
-sxl{a>1200:R,R}
 in{x<2169:lv,jtq}
 vzd{a>1040:A,m<2381:R,R}
 rfr{s<1334:A,R}
@@ -474,7 +434,6 @@ jz{s<553:R,s>856:A,R}
 rc{x>1788:qg,a>2368:bvz,ghz}
 ntn{a>1757:mm,x>3454:A,x>3057:R,R}
 gfs{m>2699:A,x<2480:A,A}
-sfz{x<413:A,A}
 ppx{s<1560:A,m<2567:A,A}
 hd{s>3328:R,A}
 vbz{a<393:A,x<579:A,A}
@@ -490,18 +449,16 @@ hnr{m<258:R,A}
 bsn{m<3351:R,s>367:mzx,R}
 kdv{m<3518:R,s<345:R,R}
 cl{m<2974:R,m>3549:R,A}
-bz{x<326:A,m<2795:ctz,gs}
+bz{x<326:A,m<2795:R,R}
 ggh{m<3295:A,m<3732:A,a>1469:A,R}
 qrd{x>685:A,a<3241:A,R}
 bh{s>1744:mv,s>958:R,m>2522:R,R}
 htj{a<1667:A,a>2011:R,A}
 nc{s>792:A,s>359:mvc,pvd}
-lqd{a<1471:R,R}
 hl{x<1415:A,R}
 lpq{m>3378:R,A}
 rqj{x<3502:A,x<3795:R,A}
-xd{s<704:slf,m>3238:nbg,vrk}
-xqt{s>3768:A,A}
+xd{s<704:R,m>3238:A,vrk}
 zsj{m<2483:R,a<3821:R,a<3848:A,A}
 cpn{s<1781:tjs,stg}
 mm{a<2038:A,m<728:A,s>2014:A,A}
@@ -820,51 +777,52 @@ var machineParts = input.Split(Environment.NewLine).Skip(workflows.Count + 1).Se
     } while (didSomething);
 }
 
-{
+//{
 
-    var didSomething = false;
+//    var didSomething = false;
 
-    do
-    {
-        didSomething = false;
+//    do
+//    {
+//        didSomething = false;
 
-        foreach (var item in workflows)
-        {
-            workflows[item.Key] = OptimizeSingle(item.Value);
-        }
+//        foreach (var item in workflows)
+//        {
+//            workflows[item.Key] = OptimizeSingle(item.Value);
+//        }
 
 
-        var singulars = workflows.Where(x => x.Value.All(x => x.next == "A" || x.next == "R") && x.Value.Count == 2)./*Take(6).*/ToList();
-        //if (singulars.Count == 1) { break; }
-        foreach (var singular in singulars)
-        {
-            foreach (var item in workflows.Except(singulars).Where(x => x.Value.Last().next == "A" || x.Value.Last().next == "R").Where(x => x.Value.TakeLast(2).First().next == singular.Key))
-            {
-                var list = item.Value;
-                if (list.Count(x => x.next == singular.Key) > 1) { throw new Exception(); }
-                var before = list.TakeWhile(x => x.next != singular.Key).ToList();
-                var after = list.Skip(before.Count+1).ToList();
-                if (!before.Any()) { continue; }
-                var x = list.Single(x => x.next == singular.Key);
-                before.Add(new Operation(x.param, x.greaterThan, x.value, singular.Value.Last().next));
+//        var singulars = workflows.Where(x => x.Value.All(x => x.next == "A" || x.next == "R")/* && x.Value.Count == 2*/).Take(82).ToList();
+//        //if (singulars.Count == 1) { break; }
+//        foreach (var singular in singulars)
+//        {
+//            foreach (var item in workflows.Except(singulars).Where(x => x.Value.Last().next == "A" || x.Value.Last().next == "R").Where(x => x.Value.TakeLast(2).First().next == singular.Key))
+//            {
+//                var list = item.Value;
+//                if (list.Count(x => x.next == singular.Key) > 1) { throw new Exception(); }
+//                var before = list.TakeWhile(x => x.next != singular.Key).ToList();
+//                var after = list.Skip(before.Count+1).ToList();
+//                if (!before.Any()) { continue; }
+//                if (!after.Any()) { continue; }
+//                var x = list.Single(x => x.next == singular.Key);
+//                before.Add(new Operation(x.param, x.greaterThan, x.value, singular.Value.Last().next));
 
-                var newList = before.Concat(singular.Value.SkipLast(1).ToList()).Concat(after).ToList();
-                workflows[item.Key] = newList;
-                didSomething = true;
-            }
-        }
-        for (int i = 0; i < singulars.Count; i++) // only replaced at the end, so still used in the middle
-        {
-            if (!workflows.Any(x => x.Value.Any(y => y.next == singulars[i].Key)))
-            {
-                workflows.Remove(singulars[i].Key);
-                didSomething = true;
-            }
-        }
-        //didSomething = false;
+//                var newList = before.Concat(singular.Value.SkipLast(1).ToList()).Concat(after).ToList();
+//                workflows[item.Key] = newList;
+//                didSomething = true;
+//            }
+//        }
+//        for (int i = 0; i < singulars.Count; i++) // only replaced at the end, so still used in the middle
+//        {
+//            if (!workflows.Any(x => x.Value.Any(y => y.next == singulars[i].Key)))
+//            {
+//                workflows.Remove(singulars[i].Key);
+//                didSomething = true;
+//            }
+//        }
+//        didSomething = false;
 
-    } while (didSomething);
-}
+//    } while (didSomething);
+//}
 
 
 
